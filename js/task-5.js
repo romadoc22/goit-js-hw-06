d60de438a20b23f71cbbe9f6af228e380e777564
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-02-25 12:45:50 +0200

    5 завдання

--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,26 @@
-const refs = document.querySelector(
-  "#name-input"
-);
+// const nameLabel = document.querySelector(
+//   "#name-input"
+// );
 
-const getName = document.querySelector(
-  "#name-output"
+// const getName = document.querySelector(
+//   "#name-output"
+// );
+
+refs = {
+  nameLabel: document.querySelector(
+    "#name-input"
+  ),
+  getName: document.querySelector("#name-output"),
+};
+
+refs.nameLabel.addEventListener(
+  "input",
+  onInputEvent
 );
 
 function onInputEvent(event) {
   refs.nameLabel.textContent =
     event.currentTarget.value === ""
       ? "Anonymous"
-      : event.currentTarget.value;
+      : refs.getName.currentTarget.value;
 }
-getName.textContent = event.currentTarget.value;
